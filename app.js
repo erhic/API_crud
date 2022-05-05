a43fe81c0f9504d2edd01209792b14f5e9a75b3a
erhic <eric.gichovi@student.moringaschool.com>
2022-05-05 15:02:34 +0300

    added a variable to store server request instances and build node server

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 // importing the http global module
 const http = require('http');
 
-// using a keyword function
+
+// ....creatting a listeners function....
+
+//1. using a keyword function
 function rqstListener(req,res){
 
 }
 http.createServer(rqstListener);
 
-// using anonymous function
+
+// 2. using anonymous function
 
 http.createServer(function(req,res){
 
-})
+});
 
-// using an arrow function
-http.createServer((req,res)=>{
 
-})
+// 3. using an arrow function + storing the instances of the request from the server
+const server = http.createServer((req,res)=>{
+console.log(req)
+});
+
+server.listen(3500);
