f5f30bc46ab573ab7834a386f845183a3516f21c
erhic <ericngugi24@gmail.com>
2023-11-12 23:44:06 +0300

    install nodemon and add a script line in package.json to run it every time change is made

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express"; // es6
-import bodyParser from 'body-parser' // allow us to tale post request body
+import bodyParser from "body-parser"; // allow us to tale post request body
 
-const app = express()// initialize our app
-const PORT = 5000
- 
-app.use(bodyParser.json())
-app.listen(PORT,()=>console.log(`Server running on port :http://localhost${PORT}`))
+const app = express(); // initialize our app
+const PORT = 5000;
+
+app.use(bodyParser.json()); //telling that will use json format data in our body
+app.listen(PORT, () =>
+  console.log(`Server running on port :http://localhost${PORT}`)
+);
