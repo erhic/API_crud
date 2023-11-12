f27378cf6966a50f1cefdafbde7f4e345b79708f
erhic <ericngugi24@gmail.com>
2023-11-12 23:37:30 +0300

    initializing our app and setting the port to listen to

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 import express from "express"; // es6
+import bodyParser from 'body-parser' // allow us to tale post request body
+
+const app = express()// initialize our app
+const PORT = 5000
+ 
+app.use(bodyParser.json())
+app.listen(PORT,()=>console.log(`Server running on port :http://localhost${PORT}`))
