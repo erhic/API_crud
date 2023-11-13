6f1e5196ce5e71355b7efeb341cf6ead36b0b565
erhic <ericngugi24@gmail.com>
2023-11-13 11:38:20 +0300

    feat : made a post request

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,27 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("ooopspps"));
+const users = [
+  {
+    firstName: "Max",
+    lastName: "Will",
+    age: 19,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Marg",
+    age: 23,
+  },
+];
+
+router.get("/", (req, res) => res.send(users));
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const usr = req.body;
+  users.push(usr);
+  console.log(usr + "users added");
+
+  res.send(`User with name ${usr.firstName} successfully added!`);
+});
 
 export default router;
