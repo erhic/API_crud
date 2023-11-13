1143d7f7cb312383a6e35b70e937f6ed8e0b3b32
erhic <ericngugi24@gmail.com>
2023-11-13 11:06:45 +0300

    feat : steting up routes on a different module wuith router method

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express"; // es6
 import bodyParser from "body-parser"; // allow us to tale post request body
+import userRoutes from "./routes/users.js";
 
 const app = express(); // initialize our app
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(bodyParser.json()); //telling that will use json format data in our body
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => res.send("hello homage "));
+
 app.listen(PORT, () =>
-  console.log(`Server running on port :http://localhost${PORT}`)
+  console.log(`Server running on port : http://localhost:${PORT}`)
 );
